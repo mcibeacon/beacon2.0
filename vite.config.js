d0cee67312f410da961b742c997ed7e564b1f1f4
kavan7 <abeyratnekavan@gmail.com>
2024-04-11 13:27:19 -0400

    base 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://mcibeacon.github.io/beacon2.0/",
   assetsInclude: ['**/*.md', '**/*.JPG']
 })
